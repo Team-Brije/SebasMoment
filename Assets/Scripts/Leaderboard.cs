9663fbec13abf23a9d3f1cb484e048c71f2b3e98
Carol <ikkilink@hotmail.com>
2025-06-02 15:46:44 -0600

    w

---
+++ Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Leaderboard : MonoBehaviour
+{
+    public List<GameObject> leaderList = new List<GameObject>();
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+  
+    }
+}
